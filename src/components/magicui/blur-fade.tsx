0e13b116869587d0b5d1c03b60b031df7101e4cb
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2025-05-23 13:34:33 +0300

    fix(wip): except ts type error

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -29,6 +29,7 @@ const BlurFade = ({
   blur = "6px",
 }: BlurFadeProps) => {
   const ref = useRef(null);
+  // @ts-expect-error: ignore margin type for now
   const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
   const isInView = !inView || inViewResult;
   const defaultVariants: Variants = {
